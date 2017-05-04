d76571e6334ede5154e00a9173cc3ec1764dfca9
Ping <ptan@fnal.gov>
2017-05-04 15:17:39 -0500

    dfladjsf

--- WZEdmAnalyzer/test/test_mc.py
+++ WZEdmAnalyzer/test/test_mc.py
@@ -1,42 +1,121 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.28 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/Powheg/Hadronizer_TuneZ2star_8TeV_generic_LHE_pythia_cff.py -s GEN --datatier GEN --eventcontent RAWSIM --conditions auto:mc --no_exec --filein file:DYToMuMuV0.lhe
 import FWCore.ParameterSet.Config as cms
-import string
 
+import FWCore.ParameterSet.VarParsing as VarParsing
 
+#options = VarParsing.VarParsing('analysis')
+#options.register('output',  '',  VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, '') 
+#options.register('infile',   '', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, '') 
+#options.parseArguments()
 
-process = cms.Process("asym")
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-#process.MessageLogger.destinations = cms.untracked.vstring("./mymessage.log")
-process.MessageLogger.suppressInfo = cms.untracked.vstring("ERROR")
-process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
-             reportEvery = cms.untracked.int32(1000),
-             limit       = cms.untracked.int32(10000000) )
+process = cms.Process('GEN')
 
-#------------------------------------------
-# Load standard sequences.
-#------------------------------------------
-process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff')
-process.load('Configuration.StandardSequences.GeometryDB_cff')
-process.load("Configuration/StandardSequences/RawToDigi_Data_cff")
-process.load("Configuration/StandardSequences/Reconstruction_cff")
-process.load('Configuration/EventContent/EventContent_cff')
+# import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.RandomNumberGeneratorService.externalLHEProducer.initialSeed = 1234
+from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper
+randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)
+randSvc.populate()
 
 
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
 
-## global tag for MC
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'START53_V27::All'
+# Input source
+process.source = cms.Source("LHESource",
+	fileNames = cms.untracked.vstring("file://eos/uscms/store/user/ptan/LHEfiles/Powheg/DYToEE_M50-8000_Z//lhe_10000_1002.lhe")
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.1 $'),
+    annotation = cms.untracked.string('runs Z2* Pythia6'),
+    name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/GenProduction/python/EightTeV/Hadronizer_TuneZ2star_8TeV_generic_LHE_pythia_cff.py,v $')
+)
+
+# Output definition
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('had.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN')
+    ),
+#    SelectEvents = cms.untracked.PSet(
+#        SelectEvents = cms.vstring('generation_step')
+#    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
+
+process.generator = cms.EDFilter("Pythia6HadronizerFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    comEnergy = cms.double(8000.0),
 
 
-
-process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(-1) )
-process.source = cms.Source("PoolSource", 
-       	fileNames = cms.untracked.vstring(
-        'file:/eos/uscms/store/user/ptan/LHEfiles/Powheg/test/test.lhe'
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.921 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.227 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model'),
+        processParameters = cms.vstring('MSEL=0          ! User defined processes', 
+            'PMAS(5,1)=4.4   ! b quark mass', 
+            'PMAS(6,1)=172.4 ! t quark mass'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
     )
 )
 
-
+#ntuplizer
 
 process.analyzer = cms.EDAnalyzer(
     "WZEdmAnalyzer",
@@ -48,7 +127,7 @@ process.analyzer = cms.EDAnalyzer(
 
 
     GeneratorLevelTag         = cms.string("generator"),
-    LHEEventProductTag        = cms.InputTag("externalLHEProducer")
+    LHEEventProductTag        = cms.InputTag("source")
     )
 
 
@@ -61,16 +140,9 @@ process.TFileService = cms.Service("TFileService",
 
 
 
-process.p = cms.Path(                     process.analyzer)
 
 
 
+#process.e = cms.EndPath(process.RAWSIMoutput)
 
-
-
-process.options = cms.untracked.PSet(
- wantSummary = cms.untracked.bool(True)
-)
-
-
-
+process.p = cms.Path(process.generator+process.randomEngineStateProducer+process.VtxSmeared+process.genParticles+process.analyzer)
